bf3e9a8c7cc3975ceefdec5db32de0c905461825
Fraser <frasermyersmusic@gmail.com>
2019-07-09 15:21:58 +1000

    version1

---
+++ first.py
@@ -0,0 +1,17 @@
+
+def calc_fact(n):
+    global i
+    fact = 1
+    for i in range(0, n):
+        fact *= i + 1
+
+    return fact
+
+
+i = 5
+
+while i > 0:
+    print("calculating: " + str(i) + " fact = ", calc_fact(i))
+
+    i -= 1
+
